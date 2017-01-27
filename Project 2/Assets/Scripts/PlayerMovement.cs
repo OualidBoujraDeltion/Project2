2e4d838edf04f63b5a4e636563e42ce743227060
OualidBoujraDeltion <mrrazozar@hotmail.com>
2017-01-27 10:43:47 +0100

    Whole Project.
    By far.

---
+++ Project 2/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public Camera mainCamera;
+    public float moveSpeed;
+    public float turnSpeed;
+
+    void Update()
+    {
+        
+        Vector3 nextDirection = gameObject.transform.position;
+        float horizontalMovement = Input.GetAxis("Horizontal");
+        float verticalMovement = Input.GetAxis("Vertical");
+
+        nextDirection += mainCamera.transform.right * horizontalMovement * moveSpeed * Time.deltaTime;
+        nextDirection += mainCamera.transform.forward * verticalMovement * moveSpeed * Time.deltaTime;
+
+        gameObject.transform.localPosition = nextDirection;
+                
+        Vector3 lookDirection = mainCamera.transform.localEulerAngles;
+        float horizontalRotation = Input.GetAxis("Mouse X");
+        float verticalRotation = Input.GetAxis("Mouse Y");
+
+        lookDirection.x -= verticalRotation * turnSpeed * Time.deltaTime;
+        lookDirection.y += horizontalRotation * turnSpeed * Time.deltaTime;
+
+        mainCamera.transform.localEulerAngles = lookDirection;
+    }
+}
